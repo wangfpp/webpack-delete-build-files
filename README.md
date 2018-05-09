b972f955c3c39f643f676bf1e94ffa7f7e17e134
wangfpp <wangfpp@gmail.com>
2018-05-09 15:28:29 +0800

    无排除文件是无需传参

--- README.md
+++ README.md
@@ -2,21 +2,24 @@
 	1.删除webpack预打包时产生的带有hash值的js文件
 	2.带有文件排除功能
 # 2.使用说明
-	2.1:安装插件
+	1.安装插件
 		npm install webpack-delete-build-files --save-dev
-	2.2:配置方法(修改webpack.dll.conf.js)
+	2.配置方法(修改webpack.dll.conf.js)<br/>
 		const removeFiles = require('webpack-delete-build-files');//引入插件
 		const perBuildDir = './static/js/';//要删除的文件目录(根据实际情况配置)
 		const excludeFiles = ['html2canvas.js','jsPdf.debug.min.js'];//要排除的文件 避免误删
+		//如果 没有需要排除的文件 这个参数就不需要传了  
 		......
 		module.exports = {
 			......
 			......
 			plugins : [
 				......
-				new removeFiles(perBuildDir,excludeFiles)
+					new removeFiles(perBuildDir,excludeFiles)//有排除文件的写法
+					new removeFiles(perBuildDir)//无排除文件的写法
 			]
 		}
+
 # 3.其他说明
 [npm地址](https://www.npmjs.com/package/webpack-delete-build-files)
 [GitHub地址](https://github.com/wangfpp/webpack-delete-build-files)
--- delete.js
+++ delete.js
@@ -2,14 +2,14 @@
 * @Author: wangfpp
 * @Date:   2018-02-28 11:11:50
 * @Last Modified by:   wangfpp
-* @Last Modified time: 2018-02-28 11:25:14
+* @Last Modified time: 2018-05-09 15:28:09
 */
 var fs = require("fs");
 var path = require("path");
 
 function DeleteBuildFiles(filePath,excludeFiles) {//filePath string 指定要删除的文件目录  excludeFiles  Array 要排除的文件
     this.filePath = filePath;
-    this.excludeFiles = excludeFiles;
+    this.excludeFiles = excludeFiles ? excludeFiles : [];
 }; 
 DeleteBuildFiles.prototype.apply = function(compile) {
     filePath = this.filePath;
@@ -18,14 +18,26 @@ DeleteBuildFiles.prototype.apply = function(compile) {
         const newlyCreatedAssets = compat.compilation.assets;
         const removeFiles = [];
         fs.readdir(path.resolve(filePath), (err, files) => {
-            files.forEach(file => {
-                if (!newlyCreatedAssets[file] && excludeFiles.indexOf(file) == -1) {
-                    fs.unlink(path.resolve(filePath + file));
-                    removeFiles.push(file);
+            if(excludeFiles.length > 0){
+                files.forEach(file => {
+                    if (!newlyCreatedAssets[file] && excludeFiles.indexOf(file) == -1) {
+                        fs.unlink(path.resolve(filePath + file));
+                        removeFiles.push(file);
+                    }
+                });
+                if (removeFiles.length > 0) {
+                    console.log('删除文件: ', removeFiles);
                 }
-            });
-            if (removeFiles.length > 0) {
-                console.log('删除文件: ', removeFiles);
+            }else{
+               files.forEach(file => {
+                    if (!newlyCreatedAssets[file]) {
+                        fs.unlink(path.resolve(filePath + file));
+                        removeFiles.push(file);
+                    }
+                });
+                if (removeFiles.length > 0) {
+                    console.log('删除文件: ', removeFiles);
+                } 
             }
         });
     });
